370cd6c8b2c2eeb44c0a824c0a1d5328b1865c89
poojareddy7766 <poojareddy9440@gmail.com>
2025-04-03 03:41:37 +0530

    ntry point for the Expense Sharing

---
+++ src/main/com/splitwise/Main.java
@@ -0,0 +1,14 @@
+package com.splitwise.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+ // Entry point for the Expense Sharing Backend application.
+@SpringBootApplication
+public class Main {
+
+  public static void main(String[] args) {
+		SpringApplication.run(Main.class, args);
+	}
+
+}
