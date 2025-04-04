ffd68289c56784675e23022b8bd58fecb5ea8c42
poojareddy7766 <poojareddy9440@gmail.com>
2025-04-04 09:16:14 +0530

    add Main.java

--- src/main/java/com/splitwise/Main.java
+++ src/main/java/com/splitwise/Main.java
@@ -1,10 +1,12 @@
-package com.splitwise.backend;
+package com.splitwise;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
- // Entry point for the Expense Sharing Backend application.
+// Entry point for the Expense Sharing Backend application.
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.splitwise.repository")
 public class Main {
 
   public static void main(String[] args) {
